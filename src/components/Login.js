4cba60735205dcdf8844e5678a8c49f027ed59a8
Richard Danon-Woedey <richard2004danon@gmail.com>
2023-05-08 13:31:53 -0400

    Update Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 //don't forget if you want to add it, and test it, just add it to app.js
-function Login(){
+function Login({ onLoginSuccess }){
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
   
